63c6ba65e63634f904a9e0e4be1e30bd1f52cdbf
Ali Shah <s.ashah@outlook.com>
2020-10-16 22:10:26 -0400

    [Add] hover-over box colour change effect

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const boxContainer = document.getElementById("box-container");
 
 for (var i = 0; i < 42; i++) {
   let box = document.createElement("div");
-  box.className = "box";
+  box.id = `box-${i + 1}`;
+  box.className = "boxes";
   boxContainer.appendChild(box);
 }
