c154c039db1ac2153114f711ab6ebd8c849f6f05
Ali Shah <s.ashah@outlook.com>
2020-10-17 23:54:38 -0400

    [Add] Primary styles and colour palette

--- script.js
+++ script.js
@@ -4,22 +4,37 @@ const boxesArray = [];
 var action = "";
 
 // -----------------------------------
-// Generates calendar skeleton
-function generateBoxes() {
-  for (var i = 0; i < 42; i++) {
-    // Make class of div elements for each box (7x6 grid)
-    // Each box has unique ID
-    let box = document.createElement("div");
-    box.id = `box-${i + 1}`;
-    box.className = "boxes";
-    boxContainer.appendChild(box);
-  
-    // Add each box to boxesArray by ID
-    let boxNumber = document.getElementById(`box-${i + 1}`);
-    boxesArray.push(boxNumber);
-  
-    // Add click listeners to each box
-    boxesArray[i].addEventListener("click", boxClick);
+// Handles clicks on boxes
+function boxClick(e) {
+  if (e.target.style.backgroundColor === "") {
+    e.target.style.backgroundColor = "green";
+    e.target.style.backgroundImage = "url(./img/+.png)";
+  } else if (e.target.style.backgroundColor === "green") {
+    e.target.style.backgroundColor = "red";
+    e.target.style.backgroundImage = "url(./img/-.png)";
+  } else if (e.target.style.backgroundColor === "red") {
+    e.target.style.backgroundColor = "yellow";
+    e.target.style.backgroundImage = "url(./img/o.png)";
+  } else if (e.target.style.backgroundColor === "yellow") {
+    e.target.style.backgroundColor = "gray";
+    e.target.style.backgroundImage = "url(./img/x.png)";
+  } else {
+    e.target.style.backgroundColor = "";
+    e.target.style.backgroundImage = "";
+  }
+}
+
+function boxClickV2(e) {
+  if (String(e.target.style.backgroundImage) === "") {
+    e.target.style.backgroundImage = "url(./img/+.png)";
+  } else if (String(e.target.style.backgroundImage) === "url(./img/+.png)") {
+    e.target.style.backgroundImage = "url(./img/-.png)";
+  } else if (e.target.style.backgroundImage === "url(./img/-.png)") {
+    e.target.style.backgroundImage = "url(./img/o.png)";
+  } else if (e.target.style.backgroundImage === "url(./img/o.png)") {
+    e.target.style.backgroundImage = "url(./img/x.png)";
+  } else {
+    e.target.style.backgroundImage = "";
   }
 }
 
@@ -36,23 +51,22 @@ function clearBoxes() {
 clearButton.addEventListener("click", clearBoxes);
 
 // -----------------------------------
-// Handles clicks on boxes
-function boxClick(e) {
-  if (e.target.style.backgroundColor === "") {
-    e.target.style.backgroundColor = "green";
-    e.target.style.backgroundImage = "url(./img/+.png)";
-  } else if (e.target.style.backgroundColor === "green") {
-    e.target.style.backgroundColor = "red";
-    e.target.style.backgroundImage = "url(./img/-.png)";
-  } else if (e.target.style.backgroundColor === "red") {
-    e.target.style.backgroundColor = "yellow";
-    e.target.style.backgroundImage = "url(./img/o.png)";
-  } else if (e.target.style.backgroundColor === "yellow") {
-    e.target.style.backgroundColor = "gray";
-    e.target.style.backgroundImage = "url(./img/na.png)";
-  } else {
-    e.target.style.backgroundColor = "";
-    e.target.style.backgroundImage = "";
+// Generates calendar skeleton
+function generateBoxes() {
+  for (var i = 0; i < 42; i++) {
+    // Make class of div elements for each box (7x6 grid)
+    // Each box has unique ID
+    let box = document.createElement("div");
+    box.id = `box-${i + 1}`;
+    box.className = "boxes";
+    boxContainer.appendChild(box);
+  
+    // Add each box to boxesArray by ID
+    let boxNumber = document.getElementById(`box-${i + 1}`);
+    boxesArray.push(boxNumber);
+  
+    // Add click listeners to each box
+    boxesArray[i].addEventListener("click", boxClick);
   }
 }
 
