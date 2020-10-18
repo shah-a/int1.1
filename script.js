24d75707d5707795efad59303db9f9d51e7d11a6
Ali Shah <s.ashah@outlook.com>
2020-10-17 22:42:48 -0400

    [Update] MVP functionality complete. Rough solution, but satisfies requirements

--- script.js
+++ script.js
@@ -1,35 +1,59 @@
 const boxContainer = document.getElementById("box-container");
+const clearButton = document.getElementById("clear");
 const boxesArray = [];
+var action = "";
 
-function boxClick(e) {
-  if (e.target.style.backgroundColor === "") {
-    e.target.style.backgroundColor = "green";
-  } else if (e.target.style.backgroundColor === "green") {
-    e.target.style.backgroundColor = "red";
-  } else if (e.target.style.backgroundColor === "red") {
-    e.target.style.backgroundColor = "yellow";
-  } else {
-    e.target.style.backgroundColor = "";
+// -----------------------------------
+// Generates calendar skeleton
+function generateBoxes() {
+  for (var i = 0; i < 42; i++) {
+    // Make class of div elements for each box (7x6 grid)
+    // Each box has unique ID
+    let box = document.createElement("div");
+    box.id = `box-${i + 1}`;
+    box.className = "boxes";
+    boxContainer.appendChild(box);
+  
+    // Add each box to boxesArray by ID
+    let boxNumber = document.getElementById(`box-${i + 1}`);
+    boxesArray.push(boxNumber);
+  
+    // Add click listeners to each box
+    boxesArray[i].addEventListener("click", boxClick);
   }
 }
 
-function boxHover(e) {
-  e.target.style.removeProperty("background-color");
+// -----------------------------------
+// Handles clicks on "clear" button
+function clearBoxes() {
+  while (boxContainer.hasChildNodes()) {
+    boxContainer.removeChild(boxContainer.firstChild);
+  }
+  boxesArray.length = 0;
+  generateBoxes();
 }
 
-for (var i = 0; i < 42; i++) {
-  // Make class of div elements for each box (7x6 grid)
-  // Each box has unique ID
-  let box = document.createElement("div");
-  box.id = `box-${i + 1}`;
-  box.className = "boxes";
-  boxContainer.appendChild(box);
+clearButton.addEventListener("click", clearBoxes);
 
-  // Add each box to boxesArray by ID
-  let boxNumber = document.getElementById(`box-${i + 1}`);
-  boxesArray.push(boxNumber);
-
-  // Add click listeners to each box
-  boxesArray[i].addEventListener("click", boxClick);
-  //boxesArray[i].addEventListener("mouseover", boxHover);
+// -----------------------------------
+// Handles clicks on boxes
+function boxClick(e) {
+  if (e.target.style.backgroundColor === "") {
+    e.target.style.backgroundColor = "green";
+    e.target.style.backgroundImage = "url(./img/+.png)";
+  } else if (e.target.style.backgroundColor === "green") {
+    e.target.style.backgroundColor = "red";
+    e.target.style.backgroundImage = "url(./img/-.png)";
+  } else if (e.target.style.backgroundColor === "red") {
+    e.target.style.backgroundColor = "yellow";
+    e.target.style.backgroundImage = "url(./img/o.png)";
+  } else if (e.target.style.backgroundColor === "yellow") {
+    e.target.style.backgroundColor = "gray";
+    e.target.style.backgroundImage = "url(./img/na.png)";
+  } else {
+    e.target.style.backgroundColor = "";
+    e.target.style.backgroundImage = "";
+  }
 }
+
+generateBoxes();
